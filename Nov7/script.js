eac20354736cb22dbdaee9f08ad94c07c434968d
saksham <sakshamshrestha400@gmail.com>
2023-11-08 07:30:44 +0545

    Nov7 commited

---
+++ Nov7/script.js
@@ -0,0 +1,45 @@
+
+// Types in JS
+// 1 primitive types
+// 2 reference types
+
+// var a = 33
+// var b
+// b = a
+// console.log(b)
+// console.log(a)
+
+// var arr1 = [1, 2, 3]
+// console.log(arr1)
+// var arr2
+// arr2 = arr1
+// console.log(arr2)
+// arr2.push(4)
+// console.log(arr2)
+// console.log(arr1)
+
+// var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// var result = arr.slice(3, 10) // kun index dekhi, kun index agadi samma
+// console.log(result)
+
+// var obj1 = {
+//   name: 'rijan',
+//   age: '20',
+// }
+// console.log(obj1)
+// var obj2 = obj1
+// console.log(obj2)
+// obj2.name = 'ram'
+// console.log(obj2)
+// console.log(obj1)
+
+// var let const diff
+// function func1() {
+//   let a = 1
+//   if (true) {
+//     let b = 2
+//     console.log(a)
+//   }
+//   console.log(b)
+// }
+// func1()
