cec44dde1c8bd9829acbe4756140f502e0319128
saksham <sakshamshrestha400@gmail.com>
2023-11-09 23:08:32 +0545

    Nov 9 updated

---
+++ Nov9/script.js
@@ -0,0 +1,52 @@
+// for each loop
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+// let func = function(a) {
+//   console.log(a)
+// }
+
+arr.forEach(function (x) {
+  console.log(x)
+})
+
+// for in loop
+// let obj = {
+//   name: 'Ram',
+//   age: 20,
+//   number: 9898989898,
+//   school: 'LRI',
+// }
+
+// console.log(obj.age)
+
+// for (var x in obj) {
+//   console.log(x + ' : ' + obj[x])
+// }
+
+// do while
+// var a = 1
+// do{
+//   console.log(a)
+//   a++
+// } while (a<5)
+
+// Callback functions
+// setTimeout(function, time)
+// function func() {
+//   console.log('hi there')
+// }
+// setTimeout(function(){
+//   console.log('okkkk')
+// }, 5000)
+
+//First class function
+// function f1(name) {
+//     console.log('hello', name)
+//   }
+//   function f2(func) {
+//     func()
+//   }
+  
+//   f2(function () {
+//     f1('saksham')
+//   })
